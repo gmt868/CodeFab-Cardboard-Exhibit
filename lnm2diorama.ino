2eb6205c81c790dac13eca5798e0d7bfd211b364
gmt868 <125gaeb@gmail.com>
2025-11-05 13:19:25 -0600

    Add files via upload

---
+++ lnm2diorama.ino
@@ -0,0 +1,42 @@
+#include <Servo.h> //include servo library
+
+// define pin numbers
+const int trigPin = 11;
+const int echoPin = 12;
+
+const int 
+
+// defines variables
+long duration;
+int distance;
+
+void setup() {
+  pinMode(trigPin, OUTPUT); // Sets the trigPin as an Output
+  pinMode(echoPin, INPUT); // Sets the echoPin as an Input
+  Serial.begin(9600); // Starts the serial communication
+
+}
+void loop() {
+//Scene 1:
+  
+
+
+
+}
+
+
+
+int distance() { //ultrasonic function
+  // Clears the trigPin
+  digitalWrite(trigPin, LOW);
+  delayMicroseconds(2);
+  // Sets the trigPin on HIGH state for 10 micro seconds
+  digitalWrite(trigPin, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(trigPin, LOW);
+  // Reads the echoPin, returns the sound wave travel time in microseconds
+  duration = pulseIn(echoPin, HIGH);
+  // Calculating the distance
+  distance = duration * 0.034 / 2;
+  // Prints the distance on the Serial Monitor
+}
